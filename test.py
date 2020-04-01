c240fdaef1d8f7586854baeea4d5cd8331789504
Isaac Craveiro <isaac.craveiro.019@gmail.com>
2020-04-01 14:03:39 +0100

    change enemy position

--- test.py
+++ test.py
@@ -7,13 +7,14 @@ WIDTH = 800
 HEIGHT = 600
 
 COLOUR = (255,123,67)
-BACKGROUND = (0,0,0)
+SILVER = (192,192,192)
+BACKGROUND = (169,169,169)
 
-player_pos = [50, 500]
+player_pos = [700, 500]
 player_size = 50
 
 enemy_size = 75
-enemy_pos = [100, 0]
+enemy_pos = [100, 25]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -37,6 +38,7 @@ while not game_over:
             player_pos = [x,y]
 
     screen.fill(BACKGROUND)
+    pygame.draw.rect(screen,  SILVER, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, COLOUR, (player_pos[0], player_pos[1], player_size, player_size))
 
     pygame.display.update()
