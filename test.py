094740e154b9dbabbf9e03596c60e43070d95fbf
Isaac Craveiro <isaac.craveiro.019@gmail.com>
2020-04-01 12:56:15 +0100

    work on keys

---
+++ test.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+COLOUR = (255,123,67)
+
+player_pos = [400, 300]
+player_size = 75
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+game_over = False
+
+while not game_over:
+
+    for event in pygame.event.get():
+        print(event)
+
+        if event.type == pygame.QUIT:
+            sys.exit(),
+
+        if event.type == pygame.KEYDOWN:
+            x = player_pos[0]
+            y = player_pos[1]
+
+            if event.key == pygame.K_LEFT:
+                x -= player_size
+            elif event.key == pygame.K_RIGHT:
+                x += player_size
+
+        player_pos = [x,y]
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, COLOUR, (player_pos[0], player_pos[1], player_size, player_size))
+
+    pygame.display.update()
