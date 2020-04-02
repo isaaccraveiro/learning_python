3156e1a60de0ff78a89a344e80add4885a63b4ec
Isaac Craveiro <isaac.craveiro.019@gmail.com>
2020-04-02 08:39:08 +0100

    created collision

--- test.py
+++ test.py
@@ -28,9 +28,12 @@ def detect_collision(player_pos, enemy_pos):
     p_y = player_pos[1]
 
     e_x = enemy_pos[0]
-    e_x = enemy_pos[1]
+    e_y = enemy_pos[1]
 
-    #f e_x >= p_x and e_x < (p_x + player_size) or(p_x >= e_x and p_x <(e_x+enemy_size))
+    if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x <(e_x+enemy_size)):
+         if (e_y >= p_y and e_y < (p_y + player_size))or (p_y >= e_y and p_x <(e_x+enemy_size)):
+            return True
+    return False
 
 while not game_over:
 
@@ -55,6 +58,10 @@ while not game_over:
     else:
         enemy_pos[0] = random.randint(0,WIDTH-enemy_size)
         enemy_pos[1] = 1
+
+    if detect_collision(player_pos,enemy_pos):
+        game_over = True
+
     pygame.draw.rect(screen,  SILVER, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, COLOUR, (player_pos[0], player_pos[1], player_size, player_size))
 
